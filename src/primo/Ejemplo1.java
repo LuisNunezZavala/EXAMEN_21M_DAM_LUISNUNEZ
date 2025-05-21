faa4f316e60b44c4f381622cce0c883a9b526730
Luis <Luis@DESKTOP-LUIS>
2025-05-21 11:20:42 +0200

    version 1

---
+++ src/primo/Ejemplo1.java
@@ -0,0 +1,18 @@
+package primo;
+
+import java.util.Scanner;
+
+public class Ejemplo1 {
+
+	public static void main(String[] args) {
+		Scanner ent = new Scanner(System.in);
+		int numero = 0;
+		
+		System.out.println("Buenos dias usuario");
+		System.out.println("Introduce un numero primo");
+		numero = Integer.parseInt(ent.nextLine());
+		
+		
+		
+	}
+}
